8f237f78bcfefe016e98f74b58e86a399761c0b2
pranay <pranaygaurav4555@gmail.com>
2022-03-16 18:47:45 +0530

    org file change

--- models/OrganizatioSchema.js
+++ models/OrganizatioSchema.js
@@ -1,14 +1,77 @@
 const mongoose = require("mongoose");
 
+var packages = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+
+  PackageName: {
+    type: String,
+    required: true,
+  },
+
+  PackageType: {
+    type: String,
+    enum: ["Package", "OT", "Bed", "Test", "Surgery", "Consultation"],
+    default: "Package",
+    required: true,
+  },
+
+  PackageDescription: {
+    type: String,
+    required: true,
+  },
+
+  image: {
+    type: String,
+    required: true,
+  },
+
+  Thumbnail: {
+    type: String,
+    required: true,
+  },
+
+  ActualPrice: {
+    type: Number,
+    required: true,
+  },
+  PortalPrice: {
+    type: Number,
+    required: true,
+  },
+  OfferPrice: {
+    type: Number,
+    required: true,
+  },
+
+  MaxPrice: {
+    type: Number,
+    required: true,
+  },
+
+  Quantity: {
+    type: Number,
+    required: true,
+  },
+
+  PaymentOption: {
+    type: String,
+    enum: ["Offline", "Online"],
+    default: "Offline",
+    required: true,
+  },
+
+  PackageStatus: {
+    type: String,
+    enum: ["Active", "Inactive"],
+    default: "Active",
+    required: true,
+  },
+});
+
 const OrganizationSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
 
-  OrgPackages: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Package",
-    },
-  ],
+  OrgPackages: [packages],
 
   OrgName: {
     type: String,
